22173925390e6deceb8aca0cabb8cfc16088fa2c
cloudtechmasters <cloudtechmasters@gmail.com>
2022-05-07 08:31:46 +0530

    added customer controller for weekend session project

---
+++ src/main/java/com/cloudtechmasters/devopsweekendsession/DevopsWeekendSessionApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtechmasters.devopsweekendsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsWeekendSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsWeekendSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudtechmasters/devopsweekendsession/DevopsWeekendSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtechmasters.devopsweekendsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsWeekendSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
